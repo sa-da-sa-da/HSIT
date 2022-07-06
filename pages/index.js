8725d7a0e91ca70a4945f319feb622167e363865
Sakaay <56340246+sa-da-sa-da@users.noreply.github.com>
2022-07-06 16:12:15 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,14 +24,14 @@ export default function Home() {
                 <div className={styles.grid}>
                     <a href="https://moodle.serverlessframe.com/"
                         className={styles.card}
-                        target="_blank">
+                    >
                         <h2>Python趣味编程入门</h2>
                         <p>Python趣味编程入门实践</p>
                     </a>
 
                     <a href="http://mk.serverlessframe.com/"
                         className={styles.card}
-                        target="_blank">
+                    >
                         <h2>Markdown 教程</h2>
                         <p>Markdown 学习知识手册</p>
                     </a>
@@ -39,7 +39,6 @@ export default function Home() {
                     <a
                         href="https://bz.sakaay.com/gz-hk-xj-B1/"
                         className={styles.card}
-                        target="_blank"
                     >
                         <h2>数据与计算</h2>
                         <p>沪科版-高中信技-必修1</p>
@@ -48,7 +47,6 @@ export default function Home() {
                     <a
                         href="https://bz.sakaay.com/gz-hk-xj-B2/"
                         className={styles.card}
-                        target="_blank"
                     >
                         <h2>信息系统与社会</h2>
                         <p>沪科版-高中信技-必修2</p>
@@ -56,7 +54,6 @@ export default function Home() {
                     <a
                         href="https://bz.sakaay.com/gz-hk-xj-X1/"
                         className={styles.card}
-                        target="_blank"
                     >
                         <h2>数据与数据结构</h2>
                         <p>沪科版-高中信技-选修1</p>
@@ -64,7 +61,6 @@ export default function Home() {
                     <a
                         href="https://bz.sakaay.com/gz-hk-xj-X2/"
                         className={styles.card}
-                        target="_blank"
                     >
                         <h2>网络基础</h2>
                         <p>沪科版-高中信技-选修2</p>
@@ -72,15 +68,13 @@ export default function Home() {
                     <a
                         href="https://bz.sakaay.com/gz-hk-xj-X3/"
                         className={styles.card}
-                        target="_blank"
                     >
                         <h2>数据管理与分析</h2>
                         <p>沪科版-高中信技-选修3</p>
                     </a>
                     <a
                         href="https://bz.sakaay.com/gz-hk-xj-X4/"
-                        className={styles.card}
-                        target="_blank"
+                        className={styles.card}      
                     >
                         <h2>人工智能初步</h2>
                         <p>沪科版-高中信技-选修4</p>
@@ -88,7 +82,6 @@ export default function Home() {
                     <a
                         href="https://bz.sakaay.com/gz-hk-xj-X5/"
                         className={styles.card}
-                        target="_blank"
                     >
                         <h2>三维设计与创意</h2>
                         <p>沪科版-高中信技-选修5</p>
@@ -96,7 +89,6 @@ export default function Home() {
                     <a
                         href="https://bz.sakaay.com/gz-hk-xj-X6/"
                         className={styles.card}
-                        target="_blank"
                     >
                         <h2>开源硬件项目设计</h2>
                         <p>沪科版-高中信技-选修6</p>
